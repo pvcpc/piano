cd7c9d8014414f66d8f50d00f47a8713a0b5ad46
Maxim Kasyanenko <maxim.kasyanenko@protonmail.ch>
2022-02-14 19:52:00 -0800

    rundemos.sh: Add optional sleep argument

--- rundemos.sh
+++ rundemos.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+SLEEP=${1:-1}
+
 for demo in $(find demos -type f -executable); do
-	echo "[$0] Heads up! Demo '$demo' up next in 1 second!"
-	sleep 1
+	echo "[$0] Heads up! Demo '$demo' up next in $SLEEP second(s)!"
+	sleep "$SLEEP"
 	"$demo"
 done
